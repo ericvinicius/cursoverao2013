6e79c2e9d6e67bea8a585b78cac0b2aa5899a798
Eric Vinicius Camargo de Campos <eric.camargo@gmail.com>
2013-01-20 23:11:40 -0200

    Exercicio da lista 3.

---
+++ lista3/l75.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main(){
+	float n1, n2;
+	int i, q1, q2, qn, q3, q4;
+
+	i = 1;
+
+	for( ;1; ){
+
+		printf("Digite o ponto %d (ou 0 0 para terminar): \n", i);
+		scanf("%f %f", &n1, &n2);
+					
+		if(n1 == 0 || n2 == 0){
+			if ((n1 == 0) && (n2 == 0)) {
+				break;
+			} 
+			qn++;
+		}
+
+		i++;
+
+		if(n1 > 0 && n2 > 0){
+			q1++;
+		} else if (n1 > 0 && n2 < 0){
+			q4++;
+		} else if(n1 < 0 && n2 < 0){
+			q3++;
+		} else if(n1 < 0 && n2 > 0) {
+			q2++;
+		}
+		
+	}  
+	if(q1 > 0){
+		printf("No primeiro quadrante temos %d. \n", q1);
+	}if(q2 > 0){
+		printf("No segundo quadrante temos %d. \n", q2);
+	}if(q3 > 0){
+		printf("No terceiro quadrante temos %d. \n", q3);
+	} if(q4 > 0){
+		printf("No quarto quadrante temos %d. \n", q4);
+	} if(qn > 0){
+		printf("%d nao estao em nenhum quadrante. \n", qn);
+	}
+
+	return 0;
+}
